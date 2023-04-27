10101f10b711b78c523d6e09520746ab553c2bbf
Savelios Stefanidis <97344586+id000000369@users.noreply.github.com>
2023-04-27 18:19:43 +0300

    add

--- src/main/java/it/mifsoft/signature/web/page/VinesPage.java
+++ src/main/java/it/mifsoft/signature/web/page/VinesPage.java
@@ -5,6 +5,7 @@ import com.vaadin.flow.router.Route;
 import com.vaadin.flow.spring.annotation.UIScope;
 import it.mifsoft.signature.web.ContentLayout;
 import it.mifsoft.signature.web.list.VineCategoriesList;
+import it.mifsoft.signature.web.list.VinesList;
 import it.mifsoft.signature.web.list.item.VineListItem;
 import it.mifsoft.signature.web.ui.VinesCarousel;
 import it.mifsoft.signature.web.utils.FlexStyleUtils;
@@ -14,31 +15,11 @@ import org.springframework.stereotype.Component;
 @UIScope
 @Route(value = "vines", layout = ContentLayout.class)
 public class VinesPage extends Div {
+   private final VinesList vinesList;
+    public VinesPage(VinesList vinesList) {
+        this.addClassNames("vines-list-div");
+        this.vinesList = vinesList;
 
-    private final VineCategoriesList categoriesList = new VineCategoriesList();
-
-    public VinesPage() {
-        this.getStyle().setPadding("40px");
-        setup();
-    }
-
-    private void setup() {
-        final Div leftSide = new Div();
-        FlexStyleUtils.doItCenteredColumn(leftSide.getElement());
-        leftSide.setWidth("50vw");
-        leftSide.setHeight("100vh");
-
-        final VinesCarousel carousel = new VinesCarousel();
-        leftSide.add(categoriesList, carousel);
-
-        final Div rightSide = new Div();
-
-        rightSide.add(new VineListItem());
-        rightSide.setWidth("50vw");
-        rightSide.setHeight("100vh");
-
-        FlexStyleUtils.doItCenteredRow(this.getElement());
-
-        this.add(leftSide, rightSide);
+        this.add(this.vinesList);
     }
 }
