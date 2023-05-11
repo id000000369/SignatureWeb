92062ec8c9d23ce8dbe71066f50459dfc7f17a35
Savelios Stefanidis <97344586+id000000369@users.noreply.github.com>
2023-05-11 21:20:08 +0300

    add

--- src/main/java/it/mifsoft/signature/web/list/item/MenuListItem.java
+++ src/main/java/it/mifsoft/signature/web/list/item/MenuListItem.java
@@ -6,28 +6,21 @@ import com.vaadin.flow.component.html.Image;
 
 public class MenuListItem extends Div {
     private final String title;
-    private final Button button;
     public MenuListItem(String title) {
         this.title = title;
-        this.button = createItemButton();
+        this.getStyle().set("width","382px");
+        this.getStyle().set("height","80px");
+        this.getStyle().set("margin-bottom","2.5%");
+        this.getStyle().set("display","flex");
+        this.getStyle().set("justify-content","center");
+        this.getStyle().set("align-items","center");
+        this.getStyle().set("border-radius","16px");
+        this.getStyle().set("font-size","20px");
+        this.getStyle().set("letter-spacing","0.07em");
+        this.getStyle().set("text-shadow","0px 4px 4px rgba(0, 0, 0, 0.25)");
+        this.getStyle().set("box-shadow","0px 2px 2px rgba(0, 0, 0, 0.25)");
+        this.getStyle().set("z-index","-1");
         this.addClassName("menu-item-main");
-        this.add(createItemButton());
-    }
-    private Button createItemButton() {
-        final Button btn = new Button();
-        btn.getStyle().set("width","382px");
-        btn.getStyle().set("height","80px");
-        btn.getStyle().set("padding-bottom","8px");
-        btn.getStyle().set("color","rgba(160, 174, 175, 1)");
-        btn.getStyle().set("border-radius","16px");
-        btn.getStyle().set("text-align","center");
-        btn.getStyle().set("font-size","20px");
-        btn.getStyle().set("letter-spacing","0.07em");
-        btn.getStyle().set("backdrop-filter","blur(12px)");
-        btn.getStyle().set("text-shadow","0px 4px 4px rgba(0, 0, 0, 0.25)");
-        btn.getStyle().set("background-image","linear-gradient(to right top, rgb(10 54 60) 0%, rgb(15 89 100) 51%, rgb(84 113 116) 100%)");
-        btn.addClassName("menu-item-button");
-        btn.setText(this.title);
-        return btn;
+        this.setText(title);
     }
 }
